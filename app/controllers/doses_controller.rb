d371411ba1a12c24efc991df406743c9bb42f9e6
ningfei ou <ouningfei@gmail.com>
2020-06-25 16:23:28 +0800

    referenced cocktail structure

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,30 @@
+class DosesController < ApplicationController
+  def destroy
+    @dose = Dose.find(params[:id])
+    cocktail = @dose.cocktail
+    @dose.destroy
+    redirect_to cocktail
+end
+
+def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+end
+
+def create
+    @dose = Dose.new(dose_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @cocktail.doses << @dose
+    if @dose.save
+        redirect_to cocktail_path(@cocktail)
+    else
+        render :new
+    end
+end
+
+private
+
+def dose_params
+    params.require(:dose).permit(:description, :cocktail_id, :ingredient_id)
+end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'doses/destroy'
+  get 'doses/new'
+  get 'doses/create'
+  root :to => 'cocktails#index'
+  resources :cocktails, only: [:index, :show, :create, :new] do
+    resources :doses, only: [:new, :create]
+  end
+  resources :doses, only: [:destroy]
 end
